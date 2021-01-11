c4a00ce29f8c28ca12c91de88926e07121626a9b
Danish <danishirfan3@gmail.com>
2021-01-11 13:46:33 -0500

    test

--- App.js
+++ App.js
@@ -1,7 +1,5 @@
 import React from "react";
-import { View } from "react-native";
 import AppNavigator from "./Components/HomePage";
-import HomePage from "./Components/HomePage";
 
 export default function App() {
   return <AppNavigator></AppNavigator>;
